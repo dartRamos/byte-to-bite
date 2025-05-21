0e0c8e97e1b33919e522b6184a75f2169e79de0d
Dartanhan <alessandroramosd@gmail.com>
2025-05-21 16:01:39 -0400

    Corrected errors in savedFavorites.ts

--- convex/functions/savedFavorites.ts
+++ convex/functions/savedFavorites.ts
@@ -3,33 +3,42 @@ import { mutation } from '../_generated/server';
 
 export const insertSavedRecipe = mutation({
   args: { 
-    userId: v.id("users"), 
     recipeId: v.number(), 
     title: v.string(),
     imageUrl: v.string(),
     isFavorited: v.boolean()
   },
 
-  handler: async (ctx, args) => { // ctx = context
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthorized.");
 
-    // checks if reciped is already saved
+    const currentUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", identity.subject))
+      .unique();
+
+    if (!currentUser) throw new Error("User not found.");
+
     const existingSavedRecipe = await ctx.db
-    .query("savedRecipes")
-    .withIndex("by_recipeId_and_userId", (q) => 
-    q.eq("recipeId", args.recipeId).eq("userId", args.userId))
-    .unique();
+      .query("savedRecipes")
+      .withIndex("by_recipeId_and_userId", (q) =>
+        q.eq("recipeId", args.recipeId).eq("userId", currentUser._id)
+      )
+      .unique();
 
-    await ctx.auth.getUserIdentity();
+    if (existingSavedRecipe) {
+      return existingSavedRecipe;
+    }
 
-    // if reciped is saved just return recipe
-    if(existingSavedRecipe) return
-
-    await ctx.db.insert("savedRecipes", {
-      userId: args.userId,
+    const savedRecipeId = await ctx.db.insert("savedRecipes", {
+      userId: currentUser._id,
       recipeId: args.recipeId,
       title: args.title,
       imageUrl: args.imageUrl,
       isFavorited: args.isFavorited
     });
+
+    return savedRecipeId;
   }
 });
