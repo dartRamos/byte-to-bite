d811328ee6ed8cc143734050760be6df8dc378ca
Tayrine Soares <tayrinecristina@hotmail.com>
2025-05-22 10:17:32 -0400

    add console log to see the saved recipes array

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { Pressable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useFavorites } from '../context/FavoritesContext' 
+import { useEffect } from 'react';
 
 // Define the expected props 
 type FavoriteButtonProps = {
   recipeId: string; // The unique ID of the recipe to toggle favorite status
 };
 
-const FavoriteButton = ({ recipeId }: FavoriteButtonProps) => {
 
+
+const FavoriteButton = ({ recipeId }: FavoriteButtonProps) => {
+  
   // Get the isFavorite checker and toggleFavorite function from context
-  const { isFavorite, toggleFavorite } = useFavorites();
+  const { isFavorite, toggleFavorite, favorites } = useFavorites();
+
+  //REMOVE THIS USEFFECT AFTER DEBUGING
+  // Log the favorites array whenever it changes
+  useEffect(() => {
+    console.log('Current Favorite Recipes:', favorites);
+  }, [favorites]);
 
   return (
     <Pressable
