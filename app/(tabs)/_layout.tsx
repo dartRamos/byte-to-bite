7db0f9dcb92037cb7223911c23c774bda0b52c01
Dartanhan <alessandroramosd@gmail.com>
2025-05-22 14:48:22 -0400

    replaced bookmark with heart for favorites

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -31,7 +31,7 @@ export default function TabLayout() {
       <Tabs.Screen name="bookmarks"
       
       options={{
-        tabBarIcon: ({size,color}) => <Ionicons name="bookmark" size={size} color={color}
+        tabBarIcon: ({size,color}) => <Ionicons name="heart" size={size} color={color}
         /> 
       }}
       />
