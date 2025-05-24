05b9343503d218e92a97cd7fc359dfdf791a04cd
Tayrine Soares <tayrinecristina@hotmail.com>
2025-05-24 14:30:48 -0400

    commit before merge

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, StyleSheet, Text } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useFavorites } from '../context/FavoritesContext' 
 
